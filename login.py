ccbb3d515adaefac497c3ecf502423d17181bc5b
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-10-19 22:32:00 +0200

    feat: add initial dialog

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QFormLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QFormLayout, QLabel, QPushButton, QCheckBox, QHBoxLayout, QDialog
 from ftplib import FTP
 from constants import default_ftp_settings
 from PyQt6.QtCore import Qt
@@ -11,24 +11,24 @@ class LoginFTPWidget(QWidget):
 
         main_layout = QVBoxLayout()
         form_box = QFormLayout()
+
         host = LabeledInput(placeholder="Host", form_box=form_box)
         port = LabeledInput(placeholder="Port", form_box=form_box, default_text='21')
         user = LabeledInput(placeholder="Użytkownik", form_box=form_box)
         password = LabeledInput(placeholder="Hasło", form_box=form_box, is_password=True)
 
-        default_settings_layout = QVBoxLayout()
+        save_login_information = QCheckBox()
+        save_login_information.setText("Zapisz dane logowania")
 
         default_settings = QLabel(f"""
-        Testowy serwer FTP:
-          - host: {default_ftp_settings["host"]}
-          - port: {default_ftp_settings["port"]}
-          - użytkownik: {default_ftp_settings["user"]}
-          - hasło: {default_ftp_settings["password"]}
+Testowy serwer FTP:
+  - host: {default_ftp_settings["host"]}
+  - port: {default_ftp_settings["port"]}
+  - użytkownik: {default_ftp_settings["user"]}
+  - hasło: {default_ftp_settings["password"]}
         """)
 
         default_settings.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-
-        default_settings_layout.addWidget(default_settings)
 
         def login():
             host_text = host.get_text()
@@ -41,9 +41,23 @@ class LoginFTPWidget(QWidget):
         login_button = QPushButton("Połącz z serwerem")
         login_button.clicked.connect(login)
 
+        select_login_information = QPushButton("Załaduj dane logowania")
+        select_login_information.clicked.connect(self.show_select_login_information)
+
+        bottom_box = QVBoxLayout()
+        bottom_box.setSpacing(20)
+        bottom_box.setContentsMargins(0, 10, 0, 10)
+
+        button_box = QHBoxLayout()
+        button_box.addWidget(select_login_information)
+        button_box.addWidget(login_button)
+
         main_layout.addLayout(form_box)
-        main_layout.addLayout(default_settings_layout)
-        main_layout.addWidget(login_button)
+        bottom_box.addWidget(save_login_information)
+        bottom_box.addLayout(button_box)
+        bottom_box.addStretch()
+        bottom_box.addWidget(default_settings)
+        main_layout.addLayout(bottom_box)
 
         self.setLayout(main_layout)
         MainWindow.setCentralWidget(self)
@@ -61,3 +75,10 @@ class LoginFTPWidget(QWidget):
         ftp.connect(host=host, port=int(port))
         ftp.login(user=user, passwd=password)
         print(ftp.pwd())
+
+    def show_select_login_information(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Załaduj dane logowania")
+        dialog.exec()
+
+
